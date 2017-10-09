b36a4a91ffa1863ecbaf229bc88c87bb832e83e1
Amy Funk <amy.y.funk@icloud.com>
2017-10-09 10:37:13 -0700

    Finished Reverse method for reverse a string

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -6,12 +6,21 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter in a word you would like to be reversed!");
+            Reverse(Console.ReadLine());
+            Console.Read();
+
         }
 
         static void Reverse(string word)
         {
+            string reversed = "";
+            for (int i = 0; i < word.Length; i--)
+            {
+                reversed += [i];
+            }
 
+            Console.WriteLine(reversed);
         }
     }
 }
