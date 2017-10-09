36a8207c91239d786aca2e33e98e150c4196ce89
Amy Funk <amy.y.funk@icloud.com>
2017-10-09 10:27:15 -0700

    Completed FizzBuzz program with method.

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the FizzBuzz Program");
+            FizzBuzz(25);
+            Console.Read();
+        }
+
+        static void FizzBuzz(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
